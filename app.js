8c1e2e9e94b91a59f0cbbadcb6c39de8a8cff96d
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-08-06 20:58:24 -0500

    added code to alternate 'X' and 'O' for each click.

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 let squares = document.querySelectorAll('.row > div');
 
-
 squares.forEach(function(square) {
   square.addEventListener('click', squareClicked);
 });
 
+let plyr = 2;
+
 function squareClicked(e) {
+  if (plyr % 2 === 0) {
   e.target.textContent = 'X';
-}
+  plyr++; 
+  } else {
+    e.target.textContent = 'O';
+    plyr--;
+    };
+}; 
