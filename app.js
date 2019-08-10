3efb3d5e1024b77728ca1f3dcccabd28d6ed149a
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-08-10 07:26:36 -0500

    trying to figure out how to get game results

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+const SYMBOLS = {
+  x: 'X',
+  o: 'O'
+}
+const RESULT = {
+  incomplete: 0,
+  playerXWon: SYMBOLS.x,
+  playerOWon: SYMBOLS.o,
+  tie: 3
+}
+
+
 let squares = document.querySelectorAll('.row > div');
 
-squares.forEach(function(square) {
+squares.forEach(function (square) {
   square.addEventListener('click', squareClicked);
 });
 
@@ -8,11 +20,12 @@ let plyr = 2;
 
 function squareClicked(e) {
   if (plyr % 2 === 0) {
-  e.target.textContent = 'X';
-  plyr++; 
+    e.target.textContent = 'X';
+    plyr++;
   } else {
     e.target.textContent = 'O';
     plyr--;
-    };
-}; 
+  };
+};
 
+    
